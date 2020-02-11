6e3ac56140d7740e8719047e84f3961e105be74c
jpaquette-student <jpaquette@wisc.edu>
2020-02-10 18:26:52 -0600

    Got input check and output working

--- PyramidWord.js
+++ PyramidWord.js
@@ -1,18 +1,17 @@
 /* 
-    JavaScript function that is meant to check if user input is a pyramid word or not.
+    JavaScript program that is meant to check if user input is a pyramid word or not.
 */
-let button = document.getElementById("button");
+window.onload = function() {
+    let button = document.getElementById("button");
 
-// Function that checks to see if userInput is valid. Then calls PyramidCheck() if
-// user entered a proper word.
-button.onclick = function() {
-    let userText = document.getElementById("userInput").value;
-    let test = /\d/;
+    button.onclick = function() {
+        let userText = document.getElementById("userInput").value;
+        let test = /\d/;
 
-    document.getElementById("result").innerHTML = 'test';
-    /*if (test.test(userText) || userText.includes(" ")) {
-        document.getElementById("result").innerHTML = "Not Valid input";
-    } else {
-        document.getElementById("result").innerHTML = "Test";
-    }*/
+        if (test.test(userText) || userText.includes(" ")) {
+            document.getElementById("result").innerHTML = "Not valid input.";
+        } else {
+            document.getElementById("result").innerHTML = "Valid";
+        }
+    }
 }
