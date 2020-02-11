20ff82293d1b465442b2503623761c7e237dee28
jpaquette-student <jpaquette@wisc.edu>
2020-02-10 18:21:14 -0600

    Still testing for onclick functionality

--- PyramidWord.js
+++ PyramidWord.js
@@ -1,16 +1,18 @@
 /* 
     JavaScript function that is meant to check if user input is a pyramid word or not.
 */
+let button = document.getElementById("button");
 
 // Function that checks to see if userInput is valid. Then calls PyramidCheck() if
 // user entered a proper word.
-function validateForm() {
+button.onclick = function() {
     let userText = document.getElementById("userInput").value;
     let test = /\d/;
 
-    if (test.test(userText) || userText.includes(" ")) {
+    document.getElementById("result").innerHTML = 'test';
+    /*if (test.test(userText) || userText.includes(" ")) {
         document.getElementById("result").innerHTML = "Not Valid input";
     } else {
         document.getElementById("result").innerHTML = "Test";
-    }
+    }*/
 }
